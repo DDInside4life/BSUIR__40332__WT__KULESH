cccd03ae2bb9a1f3831712d51ff9fda8f4b82163
DDInside4life <roman_luk99@mail.ru>
2026-03-19 18:55:36 +0300

    LB-6 (not finished)

---
+++ KULESH.Blazor/Services/ApiTeamService.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Http.Json;
+using KULESH.Domain.Entities;
+using KULESH.Domain.Models;
+
+namespace KULESH.Blazor.Services
+{
+    public class ApiTeamService(HttpClient http) : ITeamService<FootballTeam>
+    {
+        private List<FootballTeam> _teams = new();
+        private int _currentPage = 1;
+        private int _totalPages = 1;
+
+        public event Action? ListChanged;
+
+        public IEnumerable<FootballTeam> Teams => _teams;
+        public int CurrentPage => _currentPage;
+        public int TotalPages => _totalPages;
+
+        public async Task GetTeams(int pageNo = 1, int pageSize = 3)
+        {
+            try
+            {
+                var response = await http.GetAsync("api/FootballTeams");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadFromJsonAsync<ResponseData<List<FootballTeam>>>();
+                    if (responseData?.Success == true && responseData.Data != null)
+                    {
+                        var allTeams = responseData.Data;
+                        _totalPages = (int)Math.Ceiling(allTeams.Count / (double)pageSize);
+                        _currentPage = pageNo;
+                        _teams = allTeams
+                            .Skip((pageNo - 1) * pageSize)
+                            .Take(pageSize)
+                            .ToList();
+                    }
+                    else
+                    {
+                        _teams = new List<FootballTeam>();
+                        _currentPage = 1;
+                        _totalPages = 0;
+                    }
+                }
+                else
+                {
+                    _teams = new List<FootballTeam>();
+                    _currentPage = 1;
+                    _totalPages = 0;
+                }
+            }
+            catch
+            {
+                _teams = new List<FootballTeam>();
+                _currentPage = 1;
+                _totalPages = 0;
+            }
+            finally
+            {
+                ListChanged?.Invoke();
+            }
+        }
+
+        // Для совместимости с интерфейсом (если требуется)
+        public async Task<ResponseData<List<FootballTeam>>> GetTeamListAsync(string? category)
+        {
+            await GetTeams();
+            return ResponseData<List<FootballTeam>>.OK(_teams);
+        }
+
+        public Task<ResponseData<FootballTeam>> GetTeamByIdAsync(int id) => throw new NotImplementedException();
+        public Task<ResponseData<FootballTeam>> CreateTeamAsync(FootballTeam product, IFormFile? formFile) => throw new NotImplementedException();
+        public Task UpdateTeamAsync(int id, FootballTeam product, IFormFile? formFile) => throw new NotImplementedException();
+        public Task DeleteTeamAsync(int id) => throw new NotImplementedException();
+    }
+}
---
+++ KULESH.Blazor/Services/ITeamService.cs
@@ -0,0 +1,19 @@
+﻿namespace KULESH.Blazor.Services
+{
+    public interface ITeamService<T> where T : class
+    {
+        event Action ListChanged;
+
+        // List of objects
+        IEnumerable<T> Teams { get; }
+
+        // Number of actual page
+        int CurrentPage { get; }
+
+        // Total anount of pages
+        int TotalPages { get; }
+
+        // Getting the list of objects
+        Task GetTeams(int pageNo = 1, int pageSize = 3);
+    }
+}
